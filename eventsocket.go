4d2decf7cd98cdd8c408e3c4c628de9085e81bee
Ian Preston <ian@ian-preston.com>
2013-08-10 16:32:26 -0700

    replace EatResponse with ParseResponse

--- eventsocket.go
+++ eventsocket.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"net"
+	"io"
 	"bufio"
 	"strings"
+	"strconv"
 )
 
 type EventSocket struct {
@@ -41,8 +43,6 @@ func (es *EventSocket) Handle() {
 		es.conn.Close()
 		return
 	}
-
-	
 	es.conn.Close()
 }
 
@@ -99,26 +99,74 @@ func (es *EventSocket) Setup() {
 
 func (es *EventSocket) SendExecute(appName string) {
 	fmt.Fprintf(es.conn, "sendmsg\ncall-command: execute\nexecute-app-name: %s\n\n", appName)
-	es.EatResponse()
+	es.ParseResponse()
 }
 
 func (es *EventSocket) SendExecuteArg(appName string, appArg string) {
 	fmt.Fprintf(es.conn, "sendmsg\ncall-command: execute\nexecute-app-name: %s\nexecute-app-arg: %s\n\n", appName, appArg)
-	es.EatResponse()
+	es.ParseResponse()
 }
 
-func (es *EventSocket) EatResponse() error {
+func (es *EventSocket) ParseResponse() (string, error) {
+	/**
+	 * FreeSWITCH seems to return responses in one of two formats. The first, for 'execute' commands
+	 * looks like this:
+	 *
+	 * Content-Type: command/reply
+	 * Reply-Text: +OK did something
+	 *
+	 * The second, for 'api' commands, looks like thids:
+	 *
+	 * Content-Type: api/response
+	 * Content-Length: 13
+	 *
+	 * did something
+	 *
+	 * This method will determine which type of response it is and return the relevant data,
+	 * in this example, that would be either '+OK did something' or 'did something', respectively
+	 */
+	headers := make(map[string]string)
+
+	// Read in headers
 	for {
 		line, err := es.reader.ReadString('\n')
 		if err != nil {
-			return err
+			return "", err
 		}
 
 		tokens := strings.Split(line, ": ")
 		if len(tokens) < 2 {
-			return nil
+			break
 		}
+
+		key := strings.Trim(tokens[0], "\n")
+		value := strings.Trim(tokens[1], "\n")
+
+		headers[key] = value
 	}
+
+	// Handle 'command/reply' responses
+	if headers["Content-Type"] == "command/reply" {
+		return headers["Reply-Text"], nil
+	}
+
+	// Handle 'api/response' responses
+	if headers["Content-Type"] == "api/response" {
+		numBytes, err := strconv.Atoi(headers["Content-Length"])
+		if err != nil {
+			return "", err
+		}
+
+		buffer := make([]byte, numBytes)
+		_, err = io.ReadFull(es.reader, buffer)
+		if err != nil {
+			return "", err
+		}
+
+		return string(buffer), nil
+	}
+
+	return "", fmt.Errorf("Event Socket Outbound response was in an unrecognized format")
 }
 
 func (es *EventSocket) XmlApiRequest(rootUrl string) error {
